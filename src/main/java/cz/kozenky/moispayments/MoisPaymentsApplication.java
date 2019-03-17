81a796356bd10d6a6b51d9f098c9aa83894e28b2
LuZoubek <lukas.zoubek@ceskapojistovna.cz>
2019-03-17 18:45:18 +0100

    init

---
+++ src/main/java/cz/kozenky/moispayments/MoisPaymentsApplication.java
@@ -0,0 +1,13 @@
+package cz.kozenky.moispayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoisPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoisPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/kozenky/moispayments/MoisPaymentsApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.kozenky.moispayments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoisPaymentsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
