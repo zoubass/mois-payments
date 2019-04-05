e18f3f8d29bf7e942c7aec4d4d0d15a1715fc744
gabrlemilos@gmail.com <gabrlemilos@gmail.com>
2019-04-05 11:01:17 +0200

    pridana react-table

--- mois-react-ui/src/components/items/PaymentsDetail.js
+++ mois-react-ui/src/components/items/PaymentsDetail.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css"
 
 export class PaymentsDetail extends Component {
-    
+
     render() {
+        const  columns = [
+            {
+                id: 'value',
+                Header: 'Value',
+                accessor: d => d.value.amount
+            },
+            {
+                id:'currency',
+                Header: 'Currency',
+                accessor:d => d.value.currency
+            },
+            {
+                id:'categoryId',
+                Header: 'CategoryId',
+                accessor:d => d.categoryId,
+            }
+        ];
+
+        const data = this.props.paymentsData;
         return (
+
             <div>
-                <h2>PAYMENTS-DETAIL-TABLE</h2>
-                <table>
-                    <tr>
-                        <th>value</th>
-                        <th>currency</th>
-                        <th>categoryId</th>
-                    </tr>
-                    {/*vytahnuti dat, ktera byla predana z rodicovske komponenty */}
-                    {/*.map je obdoba foreach*/}
-                    {this.props.paymentsData.map(group =>
-                        <tr>
-                            <td>{group.value.amount}</td>
-                            <td>{group.value.currency}</td>
-                            <td>{group.categoryId}</td>
-                        </tr>
-                    )}
-
-                </table>
-
+                <ReactTable
+                    columns={columns}
+                    data={data}
+                    defaultPageSize={5}
+                />
             </div>
-
         );
     }
 }
