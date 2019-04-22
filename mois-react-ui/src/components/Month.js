997e6cb103f122c0302c0fd2aca7b34c6cfdff0e
gabrlmi1 <gabrlemilos@gmail.com>
2019-04-22 14:10:34 +0200

    month and year selector in {Month}

--- mois-react-ui/src/components/Month.js
+++ mois-react-ui/src/components/Month.js
@@ -3,28 +3,106 @@ import '../App.css';
 import {PieChart} from "./items/PieChart";
 import {PaymentsDetailTable} from "./items/PaymentsDetailTable";
 import {PaymentsCategoryTable} from "./items/PaymentsCategoryTable";
+import moment from 'moment'
 
 
 export class Month extends Component {
+
     state = {
         paymentsData: [],
         summaryData: [],
-        isLoading: true
+        isLoading: true,
+
+        dateFrom: '',
+        dateTo: '',
+        accountId: '',
+
+        selectMonth: '',
+        selectYear: '',
+
+        allMonths: [],
+        allYears: []
 
     };
 
     async componentDidMount() {
-        const fromDate = '01-01-1991';
-        const toDate = '29-03-2019';
+        // const yearPassed = this.props.location.state.yearPassed;
+        // const monthPassed = this.props.location.state.monthPassed;
+        const yearPassed = null;
+        const monthPassed = null;
         const accountId = 123;
 
+        var yearTo = new Date().getFullYear();
+        var yearFrom = new Date().getFullYear() - 10;
+        var year = new Date().getFullYear();
+        for (var i = yearFrom; i <= yearTo; i++) {
+            var selectedYear = false;
+            if (yearPassed && yearPassed === i) {
+                selectedYear = true;
+                year = yearPassed;
+            }
+            if (!yearPassed && i === year) {
+                selectedYear = true;
+            }
+            await this.state.allYears.push({id: i, name: i, selected: selectedYear});
+            await this.setState({selectYear: year})
+        }
+
+
+        var month = new Date().getMonth();
+        for (var j = 0; j < 12; j++) {
+            var selectedMonth = false;
+            var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            if (monthPassed && monthPassed === j) {
+                selectedMonth = true;
+                month = j;
+            }
+            if (!monthPassed && j === month) {
+                selectedMonth = true;
+            }
+            await this.state.allMonths.push({id: j, name: monthNames[j], selected: selectedMonth});
+            await this.setState({selectMonth: month})
+        }
+
+
+        var dateFrom = new Date(year, month, 1);
+        var dateTo = new Date(year, month + 1, 0);
+
+        await this.setState({
+            accountId: accountId,
+            dateTo: moment(dateTo).format('DD-MM-YYYY'),
+            dateFrom: moment(dateFrom).format('DD-MM-YYYY')
+        });
+        this.obtainData();
+    }
+
+    handleChange = async (e) => {
+        await this.setState({
+            [e.target.id]: parseInt(e.target.value)
+        });
+        console.log('stateBefore:')
+        console.log(this.state);
+        var dateFrom = new Date(this.state.selectYear, this.state.selectMonth, 1);
+        var dateTo = new Date(this.state.selectYear, this.state.selectMonth + 1, 0);
+        console.log('dateFrom: ' + dateFrom);
+        console.log('dateTo: ' + dateTo);
+        await this.setState({
+            dateTo: moment(dateTo).format('DD-MM-YYYY'),
+            dateFrom: moment(dateFrom).format('DD-MM-YYYY')
+        });
+        console.log('stateAfter:')
+        console.log(this.state);
+        this.obtainData();
+    }
+
+    async obtainData() {
         //*************************************
         //asynchroni ziskani dat z BE, cesta a parametry se nastavuji v anotaci nad danou metodou v controlleru
         //*************************************
-        const responseDetail = await fetch('/findPaymentsDetail/' + fromDate + '/' + toDate + '/' + accountId);
+        const responseDetail = await fetch('/findPaymentsDetail/' + this.state.dateFrom + '/' + this.state.dateTo + '/' + this.state.accountId);
         const bodyDetail = await responseDetail.json();
 
-        const responseSummary = await fetch('/findPaymentsSummary/' + fromDate + '/' + toDate + '/' + accountId);
+        const responseSummary = await fetch('/findPaymentsSummary/' + this.state.dateFrom + '/' + this.state.dateTo + '/' + this.state.accountId);
         const bodySummary = await responseSummary.json();
         this.setState({paymentsData: bodyDetail, summaryData: bodySummary, isLoading: false});
     }
@@ -46,6 +124,22 @@ export class Month extends Component {
         return (
             <div>
                 <h2>MONTH</h2>
+                <div className='offset-5'>
+                    <div className="input-field">
+                        <select id='selectMonth' className='custom-select-sm' onChange={this.handleChange}>
+                            {this.state.allMonths.map(month => (
+                                <option value={month.id} key={month.id} selected={month.selected}>{month.name}</option>
+                            ))}
+                        </select>
+
+                        <select id='selectYear' className='custom-select-sm' onChange={this.handleChange}>
+                            {this.state.allYears.map(year => (
+                                <option value={year.id} key={year.id} selected={year.selected}>{year.name}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+
                 <div className="col-lg">
                     <div className="card myPieChart">
                         <h4 className="card-title">Categories</h4>
@@ -60,7 +154,7 @@ export class Month extends Component {
                     <div className="col-md">
                         <div className="card">
                             <h4 className="card-title">Categories </h4>
-                            <PaymentsCategoryTable summaryData={summaryData}/>
+                            <PaymentsCategoryTable summaryData={summaryData} {...this.props}/>
                         </div>
                     </div>
 
